7630bc53ed236abe3f7c6ce000043c14fa54b3e8
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-07 09:41:36 +0800

    樣式修改

--- header.php
+++ header.php
@@ -29,8 +29,8 @@
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
 
     <link href="<?php bloginfo('template_directory') ?>/css/Flip.css" rel="stylesheet" />
-    <link href="<?php bloginfo('template_directory') ?>/css/dice.css" rel="stylesheet" />
-    <link href="<?php bloginfo('template_directory') ?>/css/style.css" rel="stylesheet" />
+    <link href="<?php bloginfo('template_directory') ?>/dice.css" rel="stylesheet" />
+    <link href="<?php bloginfo('template_directory') ?>/style.css" rel="stylesheet" />
     <link href="<?php bloginfo('template_directory') ?>/css/game_rwd.css" rel="stylesheet" />
 
 </head>
